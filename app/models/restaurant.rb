10a39c7ad2dd29e682a732dd8398eeebf7b95e77
nicolas <nicolas.eymenier@gmail.com>
2020-04-27 22:49:57 -0300

    models modified and address added to restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,11 @@
 class Restaurant < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_city_name?
+  has_many :foods, dependent: :destroy
+  belongs_to :user
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :photo, presence: true
+
 end
