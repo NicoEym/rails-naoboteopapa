824ed54f7b1e803e7b7656b8ebf590404bbb5c5d
nicolas <nicolas.eymenier@gmail.com>
2020-05-30 19:51:11 -0300

    some change and also updating the navbar depending on the user profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,28 +19,28 @@ Profile.create(name: 'Owner')
 
 
 
-csv_options = { col_sep: ';', quote_char: '"', headers: :first_row }
-filesrestaurants = 'db/csv_repos/restaurant.csv'
-filesfood = 'db/csv_repos/food.csv'
+# csv_options = { col_sep: ';', quote_char: '"', headers: :first_row }
+# filesrestaurants = 'db/csv_repos/restaurant.csv'
+# filesfood = 'db/csv_repos/food.csv'
 
-CSV.foreach(filesrestaurants, csv_options) do |row|
+# CSV.foreach(filesrestaurants, csv_options) do |row|
 
-  Restaurant.create(name: row['name'], description: row['description'], address: row['address'], photo: row['photo'] )
+#   Restaurant.create(name: row['name'], description: row['description'], address: row['address'], photo: row['photo'] )
 
-  puts "Creating Restaurant #{row['name']}"
+#   puts "Creating Restaurant #{row['name']}"
 
-end
-
-
-CSV.foreach(filesfood, csv_options) do |row|
-
-  restaurant_id = Restaurant.find_by(name: row['restaurant']).id
-  Food.create(restaurant_id: restaurant_id, name: row['name'], price: row['price'], photo: row['photo'] )
-
-  puts "Creating food #{row['name']}"
+# end
 
 
-end
+# CSV.foreach(filesfood, csv_options) do |row|
+
+#   restaurant_id = Restaurant.find_by(name: row['restaurant']).id
+#   Food.create(restaurant_id: restaurant_id, name: row['name'], price: row['price'], photo: row['photo'] )
+
+#   puts "Creating food #{row['name']}"
+
+
+# end
 
 
 
